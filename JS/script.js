7ff3b5936800bb5c2e7175f98f32b0914c7d8ca4
lukebluetiger <79377681+lukebluetiger@users.noreply.github.com>
2023-03-03 11:05:41 -0600

    new start

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+console.log('testing');
+
+function photoClick(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    const fullPage = document.querySelector('#fullpage')
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    fullPage.style.backgroundImage = 'url(' + imgs.src + ')';
+    fullPage.style.display = 'block';
+
+}
+
+function messageSent() {
+    alert("Your message has been sent!");
+}
+
+
