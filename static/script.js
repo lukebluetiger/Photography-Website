0a73a707edc59e04682e2cc0a6a9a31d1aae8629
lukebluetiger <79377681+lukebluetiger@users.noreply.github.com>
2023-03-17 19:12:26 -0500

    removed gallery from mobile (didnt make much sense)

--- static/script.js
+++ static/script.js
@@ -1,19 +1,21 @@
 
 
 function openModal() {
-    document.getElementById('my-modal').style.display = "inline-flex"; //changes display property
+    if ($(window).width() > 1025) {
+        document.getElementById('my-modal').style.display = "inline-flex"; //changes display property
 
-    // hide scrollbnar when image viewer is opened
-    document.body.style.overflow = "hidden";
-
+        // hide scrollbnar when image viewer is opened
+        document.body.style.overflow = "hidden";
+    }
 }
 
 function closeModal() {
-    document.getElementById('my-modal').style.display = "none"; //changes display property
+    if ($(window).width() > 1025) {
+        document.getElementById('my-modal').style.display = "none"; //changes display property
 
-    // hide scrollbnar when image viewer is opened
-    document.body.style.overflow = "auto";
-
+        // hide scrollbnar when image viewer is opened
+        document.body.style.overflow = "auto";
+    }
 }
 
 //declare our slide index variable
@@ -33,28 +35,31 @@ function currentSlide(n) {
 
 //Main function for image viewer
 function showSlides(n) {
-    var i;
+    if ($(window).width() > 1025) {
+        var i;
 
-    // get all elemnets with class .my-slides
-    var slidesJS = document.getElementsByClassName("my-slides");
+        // get all elemnets with class .my-slides
+        var slidesJS = document.getElementsByClassName("my-slides");
 
-    // If the slideIndexJS is greater than total no of images open first image
-    if (n > slidesJS.length) { slideIndexJS = 1; }
+        // If the slideIndexJS is greater than total no of images open first image
+        if (n > slidesJS.length) { slideIndexJS = 1; }
 
-    if (n < 1) { slideIndexJS = slideIndexJS.length; }
+        if (n < 1) { slideIndexJS = slideIndexJS.length; }
 
-    // hide all images
-    for (i = 0; i < slidesJS.length; i++) {
-        slidesJS[i].style.display = "none";
+        // hide all images
+        for (i = 0; i < slidesJS.length; i++) {
+            slidesJS[i].style.display = "none";
+        }
+
+        // Show the images whose thumbnail is clicked
+        slidesJS[slideIndexJS - 1].style.display = "block";
+
+        // show index numbers 
+        document.getElementByID('index-number').innerHTML = slidesJS[slideIndexJS - 1].getAttribute('data-index-number');
+        var imagesJS = document.getElementsByClassName("image");
+        document.getElementById('caption').innerHTML = imagesJS[slideIndexJS - 1].alt;
     }
 
-    // Show the images whose thumbnail is clicked
-    slidesJS[slideIndexJS - 1].style.display = "block";
-
-    // show index numbers 
-    document.getElementByID('index-number').innerHTML = slidesJS[slideIndexJS - 1].getAttribute('data-index-number');
-    var imagesJS = document.getElementsByClassName("image");
-    document.getElementById('caption').innerHTML = imagesJS[slideIndexJS - 1].alt;
 }
 
 function messageSent() {
