81ae13439015604899121514a4379f77a0d7f9dc
lukebluetiger <79377681+lukebluetiger@users.noreply.github.com>
2023-03-25 11:48:23 -0500

    git

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 import csv
+import git
 
 app = Flask(__name__)
 
@@ -56,6 +57,17 @@ def write_to_csv(data):
         csv_writer = csv.writer(database, delimiter=',', quotechar='"',
                                 quoting=csv.QUOTE_MINIMAL)  # using csv to write our data
         csv_writer.writerow([name, email, message])
+
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('path/to/git_repo')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 
 @app.errorhandler(404)
